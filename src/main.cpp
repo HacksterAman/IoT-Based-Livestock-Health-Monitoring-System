fbb576fac8af8bd5a028a33db7bc58085e680351
Aman Singh <as8739@srmist.edu.in>
2024-10-29 21:00:01 +0530

    modified:   platformio.ini 	modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
-#include <Arduino.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Define OLED display width and height
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Declare the display object with I2C address 0x3C
+Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup()
+{
+  // Start the display
+  if (!display.begin(0x3C))
+  {
+    Serial.println(F("SH110X OLED not found"));
+    while (1)
+      ; // Halt if display not found
+  }
+
+  display.clearDisplay();
+  display.setTextSize(1);             // Normal 1:1 pixel scale
+  display.setTextColor(SH110X_WHITE); // White text
+  display.setCursor(0, 0);            // Start at top-left corner
+  display.println(F("Hello, World!"));
+  display.display();
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  // Add more code here if you want to update the display
 }
