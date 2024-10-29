82d8f027ee5d57992bedc3dac06ed3ccacc20c94
Aman Singh <as8739@srmist.edu.in>
2024-10-29 22:27:10 +0530

    new file:   .vscode/settings.json     modified:   platformio.ini     modified:   src/main.cpp 	new file:   test/temp.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SH110X.h>
+#include <DHT.h> // Include the DHT library
 #include "MAX30100_PulseOximeter.h"
 
 #define REPORTING_PERIOD_MS 1000
@@ -13,32 +14,21 @@
 // Initialize the display
 Adafruit_SH1106G display = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
 
-// PulseOximeter is the higher-level interface to the sensor
+// PulseOximeter instance
 PulseOximeter pox;
+
+uint32_t previous = 0;
+float h, t, f;
+
+// DHT11 setup
+#define DHTPIN 4      // Pin where the DHT11 is connected
+#define DHTTYPE DHT11 // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
 
 // Callback fired when a pulse is detected
 void onBeatDetected()
 {
   Serial.println("Beat!");
-  int heartRate = pox.getHeartRate();
-  int spo2 = pox.getSpO2();
-
-  // Display data on OLED
-  display.clearDisplay();
-  display.setCursor(0, 0); // Start at top-left corner
-  display.print("Heart rate: ");
-  display.print(heartRate);
-  display.println(" bpm");
-  display.print("SpO2: ");
-  display.print(spo2);
-  display.println(" %");
-  display.display(); // Send buffer to display
-
-  Serial.print("Heart rate: ");
-  Serial.print(heartRate);
-  Serial.print(" bpm / SpO2: ");
-  Serial.print(spo2);
-  Serial.println(" %");
 }
 
 void setup()
@@ -62,19 +52,63 @@ void setup()
   pox.setOnBeatDetectedCallback(onBeatDetected);
 
   // Initialize the OLED display
-  if (!display.begin(0x3C))
-  {
-    Serial.println(F("SH110X OLED not found"));
-    for (;;)
-      ;
-  }
+  display.begin(0x3C);
+
   display.clearDisplay();
   display.setTextSize(1);             // Normal 1:1 pixel scale
   display.setTextColor(SH110X_WHITE); // White text
+
+  // Initialize DHT11 sensor
+  dht.begin();
 }
 
 void loop()
 {
   // Make sure to call update as fast as possible
   pox.update();
+  int heartRate = pox.getHeartRate();
+  int spo2 = pox.getSpO2();
+
+  if (millis() - previous > 2000)
+  {
+    // Reading t or h takes about 250 milliseconds!
+    // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+    h = dht.readHumidity();
+    // Read temperature as Celsius (the default)
+    t = dht.readTemperature();
+
+    // Check if any reads failed and exit early (to try again).
+    if (isnan(t) || isnan(h))
+    {
+      Serial.println("Failed to read from DHT sensor!");
+    }
+    previous = millis();
+  }
+
+  // Display data on OLED
+  display.clearDisplay();
+  display.setCursor(0, 0); // Start at top-left corner
+  display.print("Heart rate: ");
+  display.print(heartRate);
+  display.println(" bpm");
+  display.print("SpO2: ");
+  display.print(spo2);
+  display.println(" %");
+  display.print("Temp: ");
+  display.print(t);
+  display.println(" C");
+  display.print("Humidity: ");
+  display.print(h);
+  display.println(" %");
+  display.display(); // Send buffer to display
+
+  Serial.print("Heart rate: ");
+  Serial.print(heartRate);
+  Serial.print(" bpm / SpO2: ");
+  Serial.print(spo2);
+  Serial.print(" % / Temp: ");
+  Serial.print(t);
+  Serial.print(" C / Humidity: ");
+  Serial.print(h);
+  Serial.println(" %");
 }
